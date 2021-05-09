41cf52f63d640d3e99d9c9e9ed6cded877c292b4
BogdanSav <bohdan16@live.com>
2021-05-09 15:44:48 +0300

    buttons functional added

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,16 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {ImageServiceService} from '../image-service.service';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
+  providers: [ImageServiceService]
 })
 export class CarouselComponent implements OnInit {
+  getImages: string[];
+  carouselWrapper: any;
+  setMargin = 0;
+  constructor(imgService: ImageServiceService) {
+    this.getImages = imgService.getImages();
+  }
+  onPrev(): void{
+    this.carouselWrapper = document.querySelector('.carousel-wrapper');
+    if ( this.setMargin >= 0){
+      this.setMargin = 0;
+    }
+    else{
+      this.setMargin += 25;
+      this.carouselWrapper.style.marginLeft = this.setMargin.toString() + '%';
+    }
+  }
 
-  constructor() { }
-  images: Array<string> = ['https://cdn.pocket-lint.com/r/s/970x/assets/images/142413-apps-feature-art-and-science-collide-the-best-in-modern-space-art-image1-iha6vzu3wk-jpg.webp?v1',
-  'https://cdn.pocket-lint.com/r/s/660x/assets/images/142413-apps-feature-art-and-science-collide-the-best-in-modern-space-art-image12-4cufuy0op5-jpg.webp?v1',
-  'https://cdn.pocket-lint.com/r/s/660x/assets/images/142413-apps-feature-art-and-science-collide-the-best-in-modern-space-art-image10-jyaddp5lvj-jpg.webp?v1'];
+  onNext(): void {
+    this.carouselWrapper = document.querySelector('.carousel-wrapper');
+    if ( Math.abs(this.setMargin) > 100){
+      this.setMargin = -1 * 100;
+    }
+    this.setMargin -= 25;
+    this.carouselWrapper.style.marginLeft = this.setMargin.toString() + '%';
+
+  }
+
   ngOnInit(): void {
   }
 
--- src/app/image-service.service.ts
+++ src/app/image-service.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class ImageServiceService {
 
   constructor() { }
+  images: string[] = [1002, 101, 1015, 1019, 1018, 1023, 1029].map(ind =>  `https://picsum.photos/id/${ind}/640/480`);
+  getImages(): string[]{
+      return this.images;
+  }
 }
